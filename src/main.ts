4d6fd8b1f7b910ada20ee52b8b5b73ae107d8fa1
Sireesha Komma <sireesha9.k@gmail.com>
2025-06-05 18:53:09 -0400

    updated service worker

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
-if (environment.production) {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/ngsw-worker.js');
-  }
-}
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
